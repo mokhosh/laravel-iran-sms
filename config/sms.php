c07b7b358b2137e10e150b1402a7df3d94bb62cf
Mo Khosh <mskhoshnazar@gmail.com>
2024-02-12 17:05:09 +0330

    update config

--- config/sms.php
+++ config/sms.php
@@ -1,16 +1,12 @@
 <?php
 
 return [
-    'driver'            => env('SMS_DRIVER', 'log'),
+    'driver' => env('SMS_DRIVER', 'log'),
 
     'drivers' => [
 
-        'log' => [
-            'from' => env('SMS_LOG_FROM', ''),
-        ],
-
         'kavenegar' => [
-            'from'   => env('SMS_KAVENEGAR_FROM', ''),
+            'from' => env('SMS_KAVENEGAR_FROM', ''),
             'apiKey' => env('SMS_KAVENEGAR_KEY'),
         ],
 
