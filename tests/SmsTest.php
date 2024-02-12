52c1e2b8d755659c26bf3b0984461e9f9137e102
Mo Khosh <mskhoshnazar@gmail.com>
2024-02-12 21:08:01 +0330

    test default log driver

--- tests/SmsTest.php
+++ tests/SmsTest.php
@@ -1,10 +1,22 @@
 <?php
 
+use Illuminate\Support\Facades\Log;
 use Mokhosh\Sms\Drivers\Kavenegar;
 use Mokhosh\Sms\Enums\SmsProvider;
 use Mokhosh\Sms\Facades\Sms;
 
 use function Pest\Laravel\instance;
+
+it('logs sms by default', function () {
+    Log::shouldReceive('info')->once()
+        ->withArgs(function ($message) {
+            return str_contains($message, 'test message')
+                && str_contains($message, '09123456789');
+        });
+
+    Sms::send('test message')
+        ->to('09123456789');
+});
 
 it('can send sms using kavenegar', function () {
     $kavenegar = Mockery::mock(Kavenegar::class)->makePartial();
