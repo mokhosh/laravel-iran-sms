0eab1b9a3e3ac498a206b166ead172692554f4d1
Mo Khosh <mskhoshnazar@gmail.com>
2024-02-12 16:56:08 +0330

    remove docblock

--- src/SmsServiceProvider.php
+++ src/SmsServiceProvider.php
@@ -10,11 +10,6 @@ class SmsServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-iran-sms')
             ->hasConfigFile('sms')
