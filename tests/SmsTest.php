2486f357b2e51725583d214c9d5b39034ae9e3a7
Mo Khosh <mskhoshnazar@gmail.com>
2024-02-12 18:29:39 +0330

    add kaveneger test

---
+++ tests/SmsTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use Mokhosh\Sms\Drivers\Kavenegar;
+use Mokhosh\Sms\Enums\SmsProvider;
+use Mokhosh\Sms\Facades\Sms;
+use function Pest\Laravel\instance;
+
+it('can send sms using kavenegar', function () {
+    $kavenegar = Mockery::mock(Kavenegar::class)->makePartial();
+    $kavenegar->shouldReceive('handle')->once();
+
+    instance(Kavenegar::class, $kavenegar);
+
+    Sms::use(SmsProvider::Kavenegar)
+        ->send('test message')
+        ->to('09123456789');
+});
